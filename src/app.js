ab530484182501c10d7650c9f6e4464845d24233
NemethAngela <a.nemet75h@yahoo.com>
2023-02-16 18:05:47 +0100

    javitasok

--- src/app.js
+++ src/app.js
@@ -3,16 +3,29 @@
 * Author: Németh Angéla
 * Copyright: 2023, Németh Angéla
 * Group: Szoft 1-2 E
-* Date: 2023-02-09
+* Date: 2023-02-16
 * Github: https://github.com/NemethAngela/
 * Licenc: GNU GPL
 */
 
 const ul = document.querySelector('#lista');
 
+const gyumolcsok = [
+   
+    'szilva', 
+    'alma', 
+    'körte', 
+    'barack',
+    'eper',
+    'banán'];   //lista elem generálása
+
+gyumolcsok.forEach( (gyumolcs) => {     //ciklus törzse a zárójelben, függvényt vár a forEach, lambda a function helyett a zárójelneb, mert uaz
+    var li = document.createElement('li');
+    li.textContent = gyumolcs;
+    ul.append(li);  
+} );   
+    
+
 // li elem generálása
 
-var li = document.createElement('li');
-li.textContent = 'szilva';
-ul.append(li);    //felső sorhoz így fűzzük hozzá
 
